fb40df59acc90df16af1f148d6ecbfe0b391bad0
KatakamUmaNandini <126311990+KatakamUmaNandini@users.noreply.github.com>
2023-06-16 21:28:30 +0530

    n=int(input()) a=list(map(int,input().split())) v=[] for i in a:     k=a.count(i)     if i==k and i not in v:         v.append(i) if len(v)==0:     print("-1") else:     print(*v)

--- Elements_same_as_count.py
+++ Elements_same_as_count.py
@@ -2,10 +2,10 @@ n=int(input())
 a=list(map(int,input().split()))
 v=[]
 for i in a:
-    c=a.count(i)
-    if(c==i and i not in v):
+    k=a.count(i)
+    if i==k and i not in v:
         v.append(i)
-if(len(v)==0):
+if len(v)==0:
     print("-1")
 else:
     print(*v)
