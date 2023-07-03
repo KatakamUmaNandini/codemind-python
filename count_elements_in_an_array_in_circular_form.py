413b8d8982953b3cd5951364f8b9ad947ed2efba
KatakamUmaNandini <126311990+KatakamUmaNandini@users.noreply.github.com>
2023-07-03 16:17:59 +0530

    n=int(input()) a=list(map(int,input().split())) c=0 for i in range(n):     if i==0:         x=a[n-1]         y=a[i+1]     elif i==n-1:         x=a[i-1]         y=a[0]     else:         x=a[i-1]         y=a[i+1]     if x%2!=0 and y%2==0:         c+=1     if x%2==0 and y%2!=0:         c+=1 print(c)

--- count_elements_in_an_array_in_circular_form.py
+++ count_elements_in_an_array_in_circular_form.py
@@ -5,14 +5,14 @@ for i in range(n):
     if i==0:
         x=a[n-1]
         y=a[i+1]
-    if i==n-1:
-        y=a[0]
+    elif i==n-1:
         x=a[i-1]
+        y=a[0]
     else:
         x=a[i-1]
         y=a[i+1]
     if x%2!=0 and y%2==0:
-        c=c+1
-    if x%2==0  and y%2!=0:
-        c=c+1
+        c+=1
+    if x%2==0 and y%2!=0:
+        c+=1
 print(c)
