aa31ca3f6373ebdb065267280fbb57ff6475bd1a
KatakamUmaNandini <126311990+KatakamUmaNandini@users.noreply.github.com>
2023-07-10 10:12:14 +0530

    n=int(input()) def prime(n):     c=0     for i in range(1,n+1):         if n%i==0:             c+=1     return c k=prime(n) if k!=2:     print("Not Mega Prime") else:     c=0     co=0     while(n!=0):         t=n%10         v=prime(t)         if v==2:             c+=1         co+=1         n=n//10     if c==co:         print("Mega Prime")     else:         print("Not Mega Prime")

--- mega_prime.py
+++ mega_prime.py
@@ -1,25 +1,24 @@
 n=int(input())
-c=0
-l=[]
-for i in range(1,n+1):
-    if n%i==0:
-        c=c+1
-if(c!=2):
+def prime(n):
+    c=0
+    for i in range(1,n+1):
+        if n%i==0:
+            c+=1
+    return c
+k=prime(n)
+if k!=2:
     print("Not Mega Prime")
 else:
-    count=0
-    while n>0:
+    c=0
+    co=0
+    while(n!=0):
         t=n%10
-        l.append(t)
+        v=prime(t)
+        if v==2:
+            c+=1
+        co+=1
         n=n//10
-    for i in l:
-        co=0
-        for j in range(1,i+1):
-            if i%j==0:
-                co=co+1
-        if(co==2):
-            count=count+1
-    if len(l)==count:
+    if c==co:
         print("Mega Prime")
     else:
         print("Not Mega Prime")
