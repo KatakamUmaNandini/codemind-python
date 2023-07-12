6d9e104e084c56ef7ce5d648b1cb43597d417a91
KatakamUmaNandini <126311990+KatakamUmaNandini@users.noreply.github.com>
2023-07-12 19:55:04 +0530

    n=int(input()) def prime(n):     c=0     for i in range(1,n+1):         if n%i==0:             c+=1     return c b=[] c=[] for i in range(1,n+1):     if n%i==0:         b.append(i) for i in b:     if prime(i)==2:         c.append(i) if len(c)<2:     print("-1") else:     print(*c)

--- Find_two_distinct_prime_numbers_with_given_product.py
+++ Find_two_distinct_prime_numbers_with_given_product.py
@@ -1,5 +1,4 @@
 n=int(input())
-a=[2,3,5]
 def prime(n):
     c=0
     for i in range(1,n+1):
@@ -7,17 +6,14 @@ def prime(n):
             c+=1
     return c
 b=[]
+c=[]
 for i in range(1,n+1):
     if n%i==0:
         b.append(i)
-c=[]
 for i in b:
-    k=prime(i)
-    if k==2:
+    if prime(i)==2:
         c.append(i)
 if len(c)<2:
     print("-1")
 else:
-    print(c[0],c[1])
-
-    
+    print(*c)
